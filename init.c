5ce053384e5690762fd3d4f1c69f0456fc90d8f3
wheatfox <enkerewpo@hotmail.com>
2024-04-14 17:37:04 +0800

    cannot make zsh to run

--- init.c
+++ init.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int a[10];
+
 int main()
 {
     printf("Welcome to AXOLOS rootfs init!\n");
-    system("/zsh");
+    for (int i = 0; i < 10; i++)
+    {
+        a[i] = i;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("a[%d] = %d\n", i, a[i]);
+    }
     while (1)
         ;
 }
